4177a2472c40df82d339ea8884ee6dc408b115c6
Ali Najjar <ali.najjar@harri.com>
2026-02-16 12:49:06 +0200

    [iOS] Package dependencies upgrade (ENG-22181)  - [iOS] Upgrade JNPhoneNumberView Library (ENG-21465)    - [iOS] Upgrade JNPhoneNumberView Library (ENG-22530)

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "JNPhoneNumberView",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "JNPhoneNumberView",
+            targets: ["JNPhoneNumberView"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/iziz/libPhoneNumber-iOS.git", from: "1.0.0"),
+    ]
+    ,
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "JNPhoneNumberView",
+            dependencies: [
+                .product(name: "libPhoneNumber", package: "libPhoneNumber-iOS")
+            ],
+            resources: [
+                .process("Resources"),
+            ])
+    ]
+)
